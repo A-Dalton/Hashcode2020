f03a42be0e75cb5a82e720d041167237d9d14bc0
Thomas Lippert <thomas@twobrains.com>
2020-02-20 19:12:54 +0100

    Initial check-in with base structure and classes (without logic)

---
+++ ActualLogic.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Hashcode2k20
+{
+    class InputFile
+    {
+        public int NumberOfDifferentBooks { get; set; }
+        public int NumberOfLibraries { get; set; }
+        public int NumberOfMaxDays { get; set; }
+
+        /// <summary>
+        /// Index is ID of Book
+        /// </summary>
+        public (int Index, int Score)[] BookScores { get; set; }
+
+        public List<Library> Libraries { get; set; }
+    }
+
+    class Library
+    {
+        public int NumberOfBooksInLibrary { get; set; }
+        public int NumberOfDaysForSignup { get; set; }
+        public int NumberOfShipmentsPossiblePerDay { get; set; }
+
+        public int[] IdsOfBooksInLibrary { get; set; }
+    }
+
+
+
+
+    class ActualLogic
+    {
+        private InputFile _input = new InputFile();
+        private string _outputFile;
+        public ActualLogic(string inputFileName, string outputFolder)
+        {
+            _outputFile = Path.Combine(outputFolder, $"{Path.GetFileNameWithoutExtension(inputFileName)}.out");
+
+            string[] inputFileContents = File.ReadAllText(inputFileName).Split('\n');
+            string[] lineOne = inputFileContents[0].Split(' ');
+
+            // -->
+        }
+
+        /// <summary>
+        /// GOAL: Order as many slices as possible, but not more than the maximum number
+        /// </summary>
+        public string Process()
+        {
+            var sbOut = new StringBuilder($"\n");
+
+
+            File.WriteAllText(_outputFile, sbOut.ToString());
+            return _outputFile;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace Hashcode2k20
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string x = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            InputTextBox.Text = $"{x}\\input";
+            OutputTextBox.Text = $"{x}\\output";
+            ReadInputFiles();
+        }
+
+        private void ProcessButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Directory.Exists(OutputTextBox.Text))
+            {
+                MessageBox.Show("Output Folder does not exist");
+                return;
+            }
+
+            foreach (ListBoxItem item in FilesToProcessListBox.SelectedItems)
+            {
+                var logic = new ActualLogic((string)item.Tag, OutputTextBox.Text);
+                ProgressListBox.Items.Add($"Initalized File '{item.Content}'...");
+                string output = logic.Process();
+                ProgressListBox.Items.Add($"Wrote output to file '{output}'");
+            }
+        }
+
+        private void RefreshInputFilesButton_Click(object sender, RoutedEventArgs e) => ReadInputFiles();
+
+        private void ReadInputFiles()
+        {
+            FilesToProcessListBox.Items.Clear();
+            if (Directory.Exists(InputTextBox.Text))
+            {
+                foreach (string file in Directory.GetFiles(InputTextBox.Text))
+                    FilesToProcessListBox.Items.Add(new ListBoxItem() { Tag = file, Content = Path.GetFileName(file) });
+                FilesToProcessListBox.SelectAll();
+            }
+        }
+    }
+}
